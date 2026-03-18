61bddf9d9798eae0ff3583144ba492701ed85691
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-03-18 16:59:09 +0700

    update theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import ArchiveIcon from "@mui/icons-material/Archive";
 import { useColorScheme } from "@mui/material/styles";
+
+function SelectMode() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    setMode(event.target.value);
+    console.log(event.target.value);
+    
+  };
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="mode-select-label">Mode</InputLabel>
+      <Select
+        labelId="mode-select-label"
+        id="mode-select"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -18,6 +49,7 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <SelectMode />
       <ModeToggle />
       <div>Duong</div>
       <Button variant="text">Text</Button>
