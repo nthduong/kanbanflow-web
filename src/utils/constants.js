31b1f4f12c8b715ae24d3426f7e1b98c7a6a157d
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-05-21 10:27:22 +0700

    Fix bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ if (process.env.BUILD_MODE === "dev") {
   apiRoot = "http://localhost:8017";
 }
 if (process.env.BUILD_MODE === "prod") {
-  apiRoot = "https://kanban-api-8trx.onrender.com/";
+  apiRoot = "https://kanban-api-8trx.onrender.com";
 }
 
 export const API_ROOT = apiRoot;
