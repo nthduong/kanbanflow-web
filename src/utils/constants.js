6369fc3e8a04c6a87df3660d4deacc6669870f44
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-05-21 10:12:31 +0700

    Fix BUILD_MODE dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = "http://localhost:8017";
+let apiRoot = "";
+
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8017";
+}
+if (process.env.BUILD_MODE === "prod") {
+  apiRoot = "https://kanban-api-8trx.onrender.com/";
+}
+
+export const API_ROOT = apiRoot;
