5430527fa96840e58c24aff9e0714c0f5f6d81d8
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-06-24 19:55:05 +0700

    update production API URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,8 @@ if (process.env.BUILD_MODE === "dev") {
   apiRoot = "http://localhost:8017";
 }
 if (process.env.BUILD_MODE === "prod") {
-  apiRoot = "https://kanban-api-8trx.onrender.com";
+  // apiRoot = "https://kanban-api-8trx.onrender.com";
+  apiRoot = "https://kanban-api-production-fa5b.up.railway.app";
 }
 
 export const API_ROOT = apiRoot;
